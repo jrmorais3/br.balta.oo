f9ed19aa4dcec427d5606d2509800d38234bbb32
Junior Morais <jrmorais3@gmail.com>
2022-06-13 17:59:32 -0300

    Correções da aula 31 na classe Course

--- Balta/ContentContext/Course.cs
+++ Balta/ContentContext/Course.cs
@@ -15,18 +15,22 @@ namespace Balta.ContentContext
         }
 
         public class Module
-        { 
+        {
+            public int Order { get; set; }
+            public string Title { get; set; }
             public IList<Lecture> Lectures { get; set; }
+
             public Module()
             {
-
+                Lectures = new List<Lecture>();
             }
+
         }
 
         public class Lecture
         {
-            public string Title { get; set; }
             public int Ordem { get; set; }
+            public string Title { get; set; }
         }
 
 
